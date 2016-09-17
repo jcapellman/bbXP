1a83eec5d695913fd554ad149b6e68ba7a1ee6cc
Jarred Capellman <jcapellman@hotmail.com>
2016-09-17 09:18:30 -0400

    Corrected .NET Standard reference issues

--- Src/bbxp.BusinessLayer/Properties/AssemblyInfo.cs
+++ Src/bbxp.BusinessLayer/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("bbxp.BusinessLayer")]
+[assembly: AssemblyProduct("bbxp.WebAPI.BusinessLayer")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
