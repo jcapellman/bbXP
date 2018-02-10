d04f5ceb5ce34d538dc6e5777959cb421bdf50bf
Jarred Capellman <jcapellman@hotmail.com>
2018-02-10 10:35:43 -0600

    Change: Updated the ContentManager to use the new inmemory cache wrappers

--- Src/bbxp.web/Managers/ContentManager.cs
+++ Src/bbxp.web/Managers/ContentManager.cs
@@ -1,16 +1,24 @@
 ﻿using System.Linq;
-using System.Threading.Tasks;
 
 using bbxp.lib.Common;
 using bbxp.lib.Containers;
 using bbxp.lib.Transports.Content;
+
 using bbxp.web.DAL;
 
 namespace bbxp.web.Managers {
     public class ContentManager : BaseManager {
         public ContentManager(ManagerContainer container) : base(container) { }
 
-        public async Task<ReturnSet<ContentResponseItem>> GetContentAsync(string urlSafeName) {
+        public ReturnSet<ContentResponseItem> GetContent(string urlSafeName)
+        {
+            var (isFound, cachedResult) = GetCachedItem<ContentResponseItem>(urlSafeName);
+
+            if (isFound)
+            {
+                return new ReturnSet<ContentResponseItem>(cachedResult);
+            }
+
             using (var eFactory = new EntityFactory(mContainer.GSetings.DatabaseConnection)) {
                 var content = eFactory.Content.FirstOrDefault(a => a.URLSafename == urlSafeName && a.Active);
 
@@ -20,7 +28,7 @@ namespace bbxp.web.Managers {
 
                 var response = new ReturnSet<ContentResponseItem>(new ContentResponseItem { Body = content.Body, Title = content.Title });
 
-                await rFactory.WriteJSONAsync(content.URLSafename, response);
+                AddCachedItem(content.URLSafename, response);
 
                 return response;
             }
