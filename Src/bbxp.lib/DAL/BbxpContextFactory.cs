637cbf7330a61772f292a117a29c61c9d292de09
Jarred Capellman <jcapellman@hotmail.com>
2018-02-10 22:47:29 -0600

    Added: SQLServer Migrations

--- Src/bbxp.lib/DAL/BbxpContextFactory.cs
+++ Src/bbxp.lib/DAL/BbxpContextFactory.cs
@@ -1,11 +1,14 @@
-﻿namespace bbxp.lib.DAL
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace bbxp.lib.DAL
 {
     public class BbxpContextFactory : IDesignTimeDbContextFactory<BbxpDbContext>
     {
         public BbxpDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<BbxpDbContext>();
-            optionsBuilder.UseSqlServer("Server=localhost;Database=bbxp_jarredcapellman;user id=sa;password=bbxp4ever;");
+            optionsBuilder.UseSqlServer("Server=localhost;Database=XX;user id=XX;password=123456");
 
             return new BbxpDbContext(optionsBuilder.Options);
         }
