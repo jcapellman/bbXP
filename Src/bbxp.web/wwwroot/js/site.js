3f0c25d73083a51a06b4511402a7802f33725dc9
Jarred Capellman <jcapellman@hotmail.com>
2018-02-11 18:55:41 -0600

    Change: Updated the Colors to give the site a refreshed look

---
+++ Src/bbxp.web/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿function showLoading() {
+    $('#Loading').css("display", "block");
+    $('#LoadingContainer').css("display", "block");
+}
+
+function hideLoading() {
+    $('#Loading').css("display", "none");
+    $('#LoadingContainer').css("display", "none");
+}
