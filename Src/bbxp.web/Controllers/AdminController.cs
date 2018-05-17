11117dcc40449622fe14fdd8266f80c601992a69
Jarred Capellman <jcapellman@hotmail.com>
2018-05-16 19:49:49 -0500

    Added: Initial work on the Admin Controller/Views

---
+++ Src/bbxp.web/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using bbxp.lib.DAL;
+using bbxp.lib.Settings;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace bbxp.web.Controllers
+{
+    public class AdminController : BaseController
+    {
+        public IActionResult Login() => View();
+
+        public ActionResult Index() => View();
+
+        public ActionResult EditPost(int id) => View();
+
+        public ActionResult NewPost() => View();
+
+        public AdminController(GlobalSettings globalSettings, IMemoryCache cache, BbxpDbContext dbContext) : base(globalSettings, cache, dbContext)
+        {
+        }
+    }
+}
