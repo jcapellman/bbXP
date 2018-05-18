7d15b9453c0b74cd47412e1aa5820ab6182e033b
Jarred Capellman <jcapellman@hotmail.com>
2018-05-17 20:36:59 -0500

    Added: Continued work on the Login logic

--- Src/bbxp.web/Controllers/AdminController.cs
+++ Src/bbxp.web/Controllers/AdminController.cs
@@ -1,14 +1,51 @@
-﻿using bbxp.lib.DAL;
-using bbxp.lib.Settings;
+﻿using System.Collections.Generic;
+using System.Security.Claims;
 
+using bbxp.lib.DAL;
+using bbxp.lib.Settings;
+using bbxp.web.Models;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Memory;
 
 namespace bbxp.web.Controllers
 {
     public class AdminController : BaseController
     {
-        public IActionResult Login() => View();
+        public IActionResult Login() => View(new LoginViewModel());
+
+        private void LoginUser(int userID)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, userID.ToString())
+            };
+
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var principal = new ClaimsPrincipal(identity);
+
+            var props = new AuthenticationProperties
+            {
+                IsPersistent = true
+            };
+
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, props).Wait();
+        }
+
+        public IActionResult AttemptLogin(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View("Login", new LoginViewModel
+            {
+                ErrorString = "Could not login"
+            });
+        }
 
         public ActionResult Index() => View();
 
---
+++ Src/bbxp.web/Models/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace bbxp.web.Models
+{
+    public class LoginViewModel { 
+    
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string ErrorString { get; set; }
+    }
+}
