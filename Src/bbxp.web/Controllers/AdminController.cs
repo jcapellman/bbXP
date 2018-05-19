de4d2f4064eedf57f0a82a8f7c8bafc74dfa4c48
Jarred Capellman <jcapellman@hotmail.com>
2018-05-19 18:11:53 -0500

    Added: Initial work on CRUD operations on Posts

--- Src/bbxp.web/Controllers/AdminController.cs
+++ Src/bbxp.web/Controllers/AdminController.cs
@@ -1,5 +1,9 @@
-﻿using bbxp.lib.DAL;
+﻿using System.Threading.Tasks;
+
+using bbxp.lib.DAL;
+using bbxp.lib.Managers;
 using bbxp.lib.Settings;
+using bbxp.lib.Transports.AdminPost;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -11,12 +15,35 @@ namespace bbxp.web.Controllers
     [Authorize]
     public class AdminController : BaseController
     {
-        public ActionResult Index() => View();
+        public AdminController(BbxpDbContext dbContext, IMemoryCache cache, IOptions<GlobalSettings> globalSettings) : base(globalSettings.Value, cache, dbContext) { }
 
-        public ActionResult EditPost(int id) => View();
+        public ActionResult Index()
+        {
+            var posts = new AdminPostManager(ManagerContainer).GetPosts();
+
+            return posts.HasError ? RedirectToError(posts.ExceptionMessage) : View(posts.ReturnValue);
+        }
+
+        public ActionResult EditPost(int id)
+        {
+            var post = new AdminPostManager(ManagerContainer).GetPost(id);
+
+            return post.HasError ? RedirectToError(post.ExceptionMessage) : View(post.ReturnValue);
+        }
 
         public ActionResult NewPost() => View();
 
-        public AdminController(BbxpDbContext dbContext, IMemoryCache cache, IOptions<GlobalSettings> globalSettings) : base(globalSettings.Value, cache, dbContext) { }
+        public async Task<ActionResult> UpdatePost(AdminPostResponseItem model)
+        {
+            var result = await new AdminPostManager(ManagerContainer).UpdatePostAsync(new AdminPostRequestItem
+            {
+                Title = model.Title,
+                Body = model.Body,
+                PostID = model.ID,
+                TagList = model.Tags
+            });
+
+            return result.HasError ? RedirectToError(result.ExceptionMessage) : Index();
+        }
     }
 }
