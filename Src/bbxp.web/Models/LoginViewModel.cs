fee33ae6c8e3928f3188503c0b6a344cfc88bb77
Jarred Capellman <jcapellman@hotmail.com>
2018-05-19 10:54:01 -0500

    Added: Required Attributes on LoginViewModel

--- Src/bbxp.web/Models/LoginViewModel.cs
+++ Src/bbxp.web/Models/LoginViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace bbxp.web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bbxp.web.Models
 {
     public class LoginViewModel { 
     
+        [Required]
         public string Username { get; set; }
 
+        [Required]
         public string Password { get; set; }
 
         public string ErrorString { get; set; }
