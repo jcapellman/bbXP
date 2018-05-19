13b92042d861c8dd92d628bf2709976207e723ee
Jarred Capellman <jcapellman@hotmail.com>
2018-05-19 18:18:24 -0500

    Added: AdminPostListingItem class

---
+++ Src/bbxp.lib/Transports/AdminPost/AdminPostListingItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace bbxp.lib.Transports.AdminPost
+{
+    public class AdminPostListingItem
+    {
+        public int PostID { get; set; }
+
+        public string PostTitle { get; set; }
+
+        public DateTime PostDate { get; set; }
+    }
+}
