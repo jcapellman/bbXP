8ef39ecab4f09334299006d204c19e7e24402569
Jarred Capellman <jcapellman@hotmail.com>
2018-07-08 19:48:58 -0500

    Added: PostModel class

---
+++ Src/bbxp.web/Models/PostModel.cs
@@ -0,0 +1,11 @@
+﻿using bbxp.lib.Transports.Posts;
+
+namespace bbxp.web.Models
+{
+    public class PostModel
+    {
+        public PostResponseItem Post { get; set; }
+
+        public bool IsSinglePost { get; set; }
+    }
+}
