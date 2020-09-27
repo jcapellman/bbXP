ccd5dfb29ddf4431b6a5b3a66f9681d25093ed87
Jarred Capellman <jcapellman@hotmail.com>
2020-09-27 10:04:48 -0500

    Change: Remove site.js as it no longer is needed

--- Src/bbxp.web/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-﻿function showLoading() {
-    $('#Loading').css("display", "block");
-    $('#LoadingContainer').css("display", "block");
-}
-
-function hideLoading() {
-    $('#Loading').css("display", "none");
-    $('#LoadingContainer').css("display", "none");
-}
