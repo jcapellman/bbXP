5b66a0a3f33b44c3c53bf3d66999ef3d43590ae7
Jarred Capellman <jcapellman@hotmail.com>
2020-09-30 21:29:41 -0500

    Added: ArchiveService to the Services DI

--- Src/bbxp.blazor/Startup.cs
+++ Src/bbxp.blazor/Startup.cs
@@ -38,6 +38,7 @@ namespace bbxp.blazor
 
             services.AddSingleton<IPostService, PostService>();
             services.AddSingleton<IContentService, ContentService>();
+            services.AddSingleton<IArchiveService, ArchiveService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
