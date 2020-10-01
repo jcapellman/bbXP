40e7593bee9b03a359cba7c341e10fb32b3b6073
Jarred Capellman <jcapellman@hotmail.com>
2020-09-30 20:44:04 -0500

    Added: Initial IContentService Interface

---
+++ Src/bbxp.lib/Interfaces/IContentService.cs
@@ -0,0 +1,10 @@
+﻿using bbxp.lib.DAL;
+using bbxp.lib.Transports.Content;
+
+namespace bbxp.lib.Interfaces
+{
+    public interface IContentService
+    {
+        ContentResponseItem GetContent(BbxpDbContext context, string contentUrl);
+    }
+}
