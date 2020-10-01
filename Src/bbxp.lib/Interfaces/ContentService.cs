d676c6464a41791f41ce7cf51ee637c7e57f5048
Jarred Capellman <jcapellman@hotmail.com>
2020-09-30 20:56:20 -0500

    Added: ContentService implementation

---
+++ Src/bbxp.lib/Interfaces/ContentService.cs
@@ -0,0 +1,17 @@
+﻿using bbxp.lib.DAL;
+using bbxp.lib.Transports.Content;
+
+using System.Linq;
+
+namespace bbxp.lib.Interfaces
+{
+    public class ContentService : IContentService
+    {
+        public ContentResponseItem GetContent(BbxpDbContext context, string contentUrl)
+        {
+            var content = context.Content.FirstOrDefault(a => a.URLSafename == contentUrl);
+
+            return new ContentResponseItem { Body = content.Body, Title = content.Title };
+        }
+    }
+}
