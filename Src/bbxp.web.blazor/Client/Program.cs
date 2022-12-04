614c5899952ee273c0fe59411475de615a59ab3a
Jarred Capellman <jcapellman@hotmail.com>
2022-12-04 08:47:56 -0600

    Change: Initial work on the Blazor App

---
+++ Src/bbxp.web.blazor/Client/Program.cs
@@ -0,0 +1,20 @@
+using bbxp.web.blazor.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace bbxp.web.blazor.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
