4478cf00a110e8c12887e3984083caea87e8ff07
Jarred Capellman <jcapellman@hotmail.com>
2022-12-18 11:00:07 -0600

    Added: Additional DB Migration

--- bbxp.lib/Migrations/20221218141525_URLColumn.Designer.cs
+++ bbxp.lib/Migrations/20221218141525_URLColumn.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using bbxp.lib.Database;
@@ -11,9 +12,11 @@ using bbxp.lib.Database;
 namespace bbxp.lib.Migrations
 {
     [DbContext(typeof(bbxpDbContext))]
-    partial class bbxpDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221218141525_URLColumn")]
+    partial class URLColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ bbxp.lib/Migrations/20221218141525_URLColumn.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bbxp.lib.Migrations
+{
+    /// <inheritdoc />
+    public partial class URLColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PostDate",
+                table: "Posts",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Posts",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Posts",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddColumn<string>(
+                name: "URL",
+                table: "Posts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "URL",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PostDate",
+                table: "Posts",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Posts",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Posts",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+    }
+}
