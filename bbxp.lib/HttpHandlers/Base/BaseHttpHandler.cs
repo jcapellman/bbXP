557283190b54868142ac05b3f89daaa1d4f76316
Jarred Capellman <jcapellman@hotmail.com>
2022-12-18 11:00:59 -0600

    Added: PostHttpHandler to handle migrations and MVC App

---
+++ bbxp.lib/HttpHandlers/Base/BaseHttpHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+
+namespace bbxp.lib.HttpHandlers.Base
+{
+    public class BaseHttpHandler
+    {
+        protected readonly HttpClient httpClient;
+
+        public BaseHttpHandler(string baseAddress)
+        {
+            httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
+        }
+
+        protected async Task<bool> PostAsync<T>(string url, T objValue)
+        {
+            var result = await httpClient.PostAsJsonAsync<T>(url, objValue);
+
+            return result != null && result.IsSuccessStatusCode;
+        }
+
+        protected async Task<T> GetAsync<T>(string url) => await httpClient.GetFromJsonAsync<T>(url);
+    }
+}
---
+++ bbxp.lib/HttpHandlers/PostHttpHandler.cs
@@ -0,0 +1,17 @@
+﻿using bbxp.lib.Common;
+using bbxp.lib.Database.Tables;
+using bbxp.lib.HttpHandlers.Base;
+using bbxp.lib.JSON;
+
+namespace bbxp.lib.HttpHandlers
+{
+    public class PostHttpHandler : BaseHttpHandler
+    {
+        public PostHttpHandler(string baseAddress) : base(baseAddress) { }
+
+        public async Task<bool> CreateNewPost(PostCreationRequestItem newPost) => await PostAsync("posts", newPost);
+
+        public async Task<List<Posts>> GetPostsAsync(string category = AppConstants.POST_REQUEST_DEFAULT_CATEGORY, int postCountLimit = AppConstants.POST_REQUEST_DEFAULT_LIMIT) 
+            => await GetAsync<List<Posts>>($"posts/{category}/{postCountLimit}");
+    }
+}
