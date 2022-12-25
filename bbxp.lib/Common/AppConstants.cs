b7362ae98eb6d929d97eee20b24b02dd73eea132
jcapellman <jcapellman@hotmail.com>
2022-12-25 18:52:37 -0500

    Added: Constant for Post Category cache

--- bbxp.lib/Common/AppConstants.cs
+++ bbxp.lib/Common/AppConstants.cs
@@ -7,5 +7,7 @@
         public const int POST_REQUEST_DEFAULT_LIMIT = 10;
 
         public const string POST_REQUEST_DEFAULT_CATEGORY = "Latest";
+
+        public const string POST_CATEGORY_CACHE_KEY = "PostCategories";
     }
 }
