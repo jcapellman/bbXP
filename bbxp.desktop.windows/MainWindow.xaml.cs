424d12beee641d72bb53c50fa84f2637694ed025
Jarred Capellman <jcapellman@hotmail.com>
2023-01-01 15:17:34 -0600

    Added: Initial checkin of the WPF Admin App

---
+++ bbxp.desktop.windows/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using bbxp.desktop.windows.ViewModels;
+
+using System.Windows;
+
+namespace bbxp.desktop.windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MainViewModel();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ bbxp.desktop.windows/ViewModels/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using bbxp.desktop.windows.ViewModels.Base;
+using bbxp.lib.Database.Tables;
+using bbxp.lib.HttpHandlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bbxp.desktop.windows.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private const string REST_SERVICE_BASE_URL = "";
+
+        private Posts _selectedPost;
+
+        public Posts SelectedPost
+        {
+            get => _selectedPost;
+
+            set
+            {
+                _selectedPost = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private List<Posts> _posts;
+
+        public List<Posts> Posts
+        {
+            get => _posts;
+
+            set
+            {
+                _posts = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Posts = new List<Posts>();
+
+            LoadData();
+        }
+
+        public async void LoadData()
+        {
+            Posts = await new PostHttpHandler(REST_SERVICE_BASE_URL).GetPostsAsync(postCountLimit: int.MaxValue);
+
+            if (Posts != null && Posts.Count > 0)
+            {
+                SelectedPost = Posts.FirstOrDefault();
+            }
+        }
+    }
+}
