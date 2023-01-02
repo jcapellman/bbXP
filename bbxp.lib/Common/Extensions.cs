71c318f6c342a229cccfec6afec4761187b1a0ff
jcapellman <jcapellman@hotmail.com>
2023-01-01 22:43:59 -0600

    Fix: Dashes are now removed the SHA256

--- bbxp.lib/Common/Extensions.cs
+++ bbxp.lib/Common/Extensions.cs
@@ -5,6 +5,6 @@ namespace bbxp.lib.Common
 {
     public static class Extensions
     {
-        public static string ToSHA256(this string str) => BitConverter.ToString(SHA256.HashData(Encoding.ASCII.GetBytes(str))).ToUpper();
+        public static string ToSHA256(this string str) => BitConverter.ToString(SHA256.HashData(Encoding.ASCII.GetBytes(str))).Replace("-","").ToUpper();
     }
 }
