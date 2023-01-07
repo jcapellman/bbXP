26a9194173c86e21bb93d5c72e11b5be35e6bc75
jcapellman <jcapellman@hotmail.com>
2023-01-07 11:31:13 -0600

    Added: Character Limiter in the Post Title

---
+++ bbxp.desktop.windows/Converters/CharacterLimitConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace bbxp.desktop.windows.Converters
+{
+    public class CharacterLimitConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string input = (string)value;
+
+            int charLimit = int.Parse((string)parameter);
+
+            if (input.Length > charLimit)
+            {
+                return string.Concat(input.AsSpan(0, charLimit), "...");
+            }
+
+            return input;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => (string)value;
+    }
+}
