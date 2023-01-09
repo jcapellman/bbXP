725b99c6b57405fafeaa3fb0a44f01b93c5679a0
jcapellman <jcapellman@hotmail.com>
2023-01-08 19:53:06 -0600

    Added: Optional Markdown rendering #23

---
+++ bbxp.web.mvc/Common/MarkdownExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Html;
+
+namespace bbxp.web.mvc.Common
+{
+    public static class MarkdownExtension
+    {
+        public static HtmlString ToHtmlString(this string markdown) 
+            => new(Markdig.Markdown.ToHtml(markdown));
+    }
+}
