6608d12ae663ba1c3db24b61d9d9a4ea3235a0f4
Jarred Capellman <jcapellman@hotmail.com>
2024-04-28 17:56:02 -0500

    Added: Environment Variable Update

--- bbxp.web.mvc/Program.cs
+++ bbxp.web.mvc/Program.cs
@@ -8,6 +8,8 @@ namespace bbxp.web.mvc
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Configuration.AddEnvironmentVariables();
+
             builder.Services.AddControllersWithViews();
             builder.Services.Configure<AppConfiguration>(builder.Configuration.GetSection(nameof(AppConfiguration)));
             
@@ -15,11 +17,9 @@ namespace bbxp.web.mvc
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
