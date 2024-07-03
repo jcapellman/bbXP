54c892e70d16737685eec2705dbeb082d1044bd0
Jarred Capellman <jcapellman@hotmail.com>
2024-07-02 22:17:45 -0500

    Change:  Streamlined the PostSearchController

--- bbxp.web.api/Controllers/PostSearchController.cs
+++ bbxp.web.api/Controllers/PostSearchController.cs
@@ -3,16 +3,18 @@ using bbxp.lib.Database.Tables;
 using bbxp.web.api.Controllers.Base;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace bbxp.web.api.Controllers
 {
     [ApiController]
     [Route("api/postsearch")]
-    public class PostSearchController(BbxpContext dbContext, IMemoryCache memoryCache) : BaseController(dbContext, memoryCache)
+    public class PostSearchController(BbxpContext dbContext, IMemoryCache memoryCache) : BaseController(memoryCache)
     {
         [HttpGet]
         [Route("{searchQuery}")]
-        public IEnumerable<Posts> SearchPosts([FromRoute] string searchQuery) => GetPostsFromSearchAsync(searchQuery);
+        public async Task<List<Posts>> SearchPostsAsync([FromRoute] string searchQuery) => await dbContext.Posts.Where(a => a.Title.Contains(searchQuery, StringComparison.CurrentCultureIgnoreCase)).ToListAsync();
+     
     }
 }
