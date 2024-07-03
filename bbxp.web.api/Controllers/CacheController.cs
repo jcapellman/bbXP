53baebe3639c59aa9faffe11de5db89b11ca29c5
Jarred Capellman <jcapellman@hotmail.com>
2024-07-02 22:07:50 -0500

    Fix: CacheController is now authenticated

--- bbxp.web.api/Controllers/CacheController.cs
+++ bbxp.web.api/Controllers/CacheController.cs
@@ -1,6 +1,7 @@
 ﻿using bbxp.lib.Database;
 using bbxp.web.api.Controllers.Base;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 
@@ -10,6 +11,7 @@ namespace bbxp.web.api.Controllers
     [Route("api/posts")]
     public class CacheController(BbxpContext dbContext, IMemoryCache memoryCache) : BaseController(dbContext, memoryCache)
     {
+        [Authorize]
         [HttpGet]
         public void FlushCache()
         {
