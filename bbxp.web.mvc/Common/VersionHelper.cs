d39cd0da082f5322e6e985f5784b0cb45bbc3fc7
Jarred Capellman <jcapellman@hotmail.com>
2024-07-03 13:57:56 -0700

    Added: Version Display from the Assembly

---
+++ bbxp.web.mvc/Common/VersionHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace bbxp.web.mvc.Common
+{
+    public static class VersionHelper
+    {
+        public static string GetAssemblyVersion()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown Version";
+        }
+    }
+}
