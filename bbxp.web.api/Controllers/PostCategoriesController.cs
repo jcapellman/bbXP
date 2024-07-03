2fac2c38f7839a385cb71da7741eb1cf6e2d2b7e
Jarred Capellman <jcapellman@hotmail.com>
2024-07-02 22:34:26 -0500

    Change: Cleaned up the logic in the PostCategoriesController

--- bbxp.web.api/Controllers/PostCategoriesController.cs
+++ bbxp.web.api/Controllers/PostCategoriesController.cs
@@ -1,16 +1,33 @@
-﻿using bbxp.lib.Database;
+﻿using bbxp.lib.Common;
+using bbxp.lib.Database;
 using bbxp.web.api.Controllers.Base;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace bbxp.web.api.Controllers
 {
     [ApiController]
     [Route("api/postcategories")]
-    public class PostCategoriesController(BbxpContext dbContext, IMemoryCache memoryCache) : BaseController(dbContext, memoryCache)
+    public class PostCategoriesController(BbxpContext dbContext, IMemoryCache memoryCache) : BaseController(memoryCache)
     {
         [HttpGet]
-        public async Task<List<string>> GetPostCategoriesAsync() => await GetCategoriesAsync();
+        public async Task<List<string>> GetPostCategoriesAsync()
+        {
+            var cacheResult = GetFromCache<List<string>>(AppConstants.POST_REQUEST_DEFAULT_CATEGORY);
+
+            if (cacheResult is not null)
+            {
+                return cacheResult;
+            }
+
+            var dbResult = await dbContext.Posts.Where(a => a.Active &&
+                                                         a.Category != AppConstants.POST_REQUEST_DEFAULT_CATEGORY &&
+                                                         a.Category != AppConstants.POST_REQUEST_INTERNAL_CATEGORY)
+                .Select(a => a.Category).Distinct().OrderBy(a => a).ToListAsync();
+
+            return AddToCache(AppConstants.POST_REQUEST_DEFAULT_CATEGORY, dbResult);
+        }
     }
 }
