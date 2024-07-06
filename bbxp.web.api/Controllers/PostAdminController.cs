02fd0760f6bb7e92f7bac9d9de2ee17aa0e8d71f
Jarred Capellman <jcapellman@hotmail.com>
2024-07-06 08:29:13 -0700

    Change: Moved the Post Admin CRUD operations into it's own controller

---
+++ bbxp.web.api/Controllers/PostAdminController.cs
@@ -0,0 +1,98 @@
+﻿using bbxp.lib.Database.Tables;
+using bbxp.lib.JSON;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using bbxp.lib.Common;
+using bbxp.lib.Database;
+
+using bbxp.web.api.Controllers.Base;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace bbxp.web.api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/post-admin")]
+    public class PostAdminController(BbxpContext dbContext, IMemoryCache memoryCache, ILogger<PostAdminController> logger) : BaseController(memoryCache)
+    {
+        [HttpPatch]
+        public async Task<bool> UpdateAsync(PostUpdateRequestItem updatePost)
+        {
+            try
+            {
+                var post = dbContext.Posts.FirstOrDefault(a => a.Id == updatePost.Id);
+
+                if (post is null)
+                {
+                    return false;
+                }
+
+                post.Title = updatePost.Title;
+                post.Body = updatePost.Body;
+                post.Category = updatePost.Category;
+                post.PostDate = updatePost.PostDate;
+                post.URL = updatePost.URL;
+
+                return await dbContext.SaveChangesAsync() > 0;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Failed to Update Post due to {ex}", ex);
+
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public async Task<bool> AddAsync(PostCreationRequestItem newPost)
+        {
+            try
+            {
+                var post = new Posts
+                {
+                    PostDate = newPost.PostDate ?? DateTime.Now,
+                    Body = newPost.Body,
+                    Title = newPost.Title,
+                    Category = newPost.Category,
+                    URL = newPost.Title.ToUrlSafeTitle()
+                };
+
+                dbContext.Posts.Add(post);
+
+                return await dbContext.SaveChangesAsync() > 0;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Failed to Create Post due to {ex}", ex);
+
+                throw;
+            }
+        }
+
+        [HttpDelete]
+        public async Task<bool> DeleteAsync(int postId)
+        {
+            try
+            {
+                var post = await dbContext.Posts.FirstAsync(a => a.Id == postId);
+
+                if (post == null)
+                {
+                    return false;
+                }
+
+                post.Active = false;
+
+                return await dbContext.SaveChangesAsync() > 0;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Failed to Delete Post due to {ex}", ex);
+
+                throw;
+            }
+        }
+    }
+}
