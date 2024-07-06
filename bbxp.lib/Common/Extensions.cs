d7503164254e131a9a1b6fb60f6b154c61cc6193
Jarred Capellman <jcapellman@hotmail.com>
2024-07-06 08:28:53 -0700

    Change: Moved the SafeURL Method to an Extension Method

--- bbxp.lib/Common/Extensions.cs
+++ bbxp.lib/Common/Extensions.cs
@@ -6,5 +6,9 @@ namespace bbxp.lib.Common
     public static class Extensions
     {
         public static string ToSHA256(this string str) => BitConverter.ToString(SHA256.HashData(Encoding.ASCII.GetBytes(str))).Replace("-","").ToUpper();
+
+
+        public static string ToUrlSafeTitle(this string title) =>
+            title.ToLower().Replace(' ', '_').Replace(".", "").Replace(",", "").Replace("-", "_");
     }
 }
