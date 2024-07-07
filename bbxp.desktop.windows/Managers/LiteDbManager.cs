63213982e7d15f3e056afaa4a86c140d48e8da6f
Jarred Capellman <jcapellman@hotmail.com>
2024-07-06 19:30:05 -0700

    Added: Initial work on the LiteDbManager class

---
+++ bbxp.desktop.windows/Managers/LiteDbManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using bbxp.lib.Database.Tables;
+using bbxp.desktop.windows.Common;
+
+namespace bbxp.desktop.windows.Managers
+{
+    public class LiteDbManager
+    {
+        public static IEnumerable<Posts>? GetLocalPosts()
+        {
+            using var db = new LiteDB.LiteDatabase(AppConstants.DB_FILENAME);
+
+            var collection = db.GetCollection<Posts>();
+
+            if (collection is null)
+            {
+                return null;
+            }
+
+            return collection.FindAll();
+        }
+
+        public static DateTime GetMostRecentPostUpdate()
+        {
+            using var db = new LiteDB.LiteDatabase(AppConstants.DB_FILENAME);
+
+            var collection = db.GetCollection<Posts>();
+
+            if (collection is null)
+            {
+                return DateTime.MinValue;
+            }
+
+            return collection.Max(a => a.Modified);
+        }
+    }
+}
