2e7a73b257f9ecd3fc6aa0a48131c79068dc0692
Jarred Capellman <jcapellman@hotmail.com>
2024-07-07 16:11:11 -0500

    Change: Cleaned up the example app

--- bbxp.web.admin/Program.cs
+++ bbxp.web.admin/Program.cs
@@ -8,17 +8,15 @@ namespace bbxp.web.admin
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
 
+            builder.Services.AddHttpClient();
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
